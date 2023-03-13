2fbc5f0db6e94d0abe10ab5c4210c472bcda043e
luis-viegas <luiscarlosviegas@icloud.com>
2023-03-13 18:00:10 +0000

    backend changes

--- backend/api/search/[searchTerm].js
+++ backend/api/search/[searchTerm].js
@@ -53,6 +53,6 @@ export default async function handler(req, res) {
 
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.json({ athlete: JSON.parse(JSON.stringify(data)) });
+    res.json(JSON.parse(JSON.stringify(data)));
   }
 }
