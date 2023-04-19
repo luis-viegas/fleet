34290c93e600fbfd3d6592ebb97c0105a7183606
luis-viegas <luiscarlosviegas@icloud.com>
2023-04-19 14:03:08 +0100

    [bug] backend events now support number or string as id

--- backend/src/routes/api/event/[event_id]/+server.ts
+++ backend/src/routes/api/event/[event_id]/+server.ts
@@ -2,10 +2,16 @@ import { events } from '$db/events';
 import { json } from '@sveltejs/kit';
 
 export async function GET({ params }) {
-	const eventObj = await events.findOne(
-		{ fpa_id: Number(params.event_id) },
+	let eventObj = await events.findOne(
+		{ fpa_id: String(params.event_id) },
 
 	);
+	if (!eventObj) {
+		eventObj = await events.findOne(
+			{ fpa_id: Number(params.event_id) },
+	
+		);
+	}
 
 	const data = eventObj
 
