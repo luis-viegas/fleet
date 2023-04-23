6296768265b9d5823a80f0704b28e8cd7e2dd03e
luis-viegas <luiscarlosviegas@icloud.com>
2023-04-23 13:22:39 +0100

    upcoming events now limited to 40 instead of 20

--- backend/src/routes/api/homepage/events/upcoming/+server.ts
+++ backend/src/routes/api/homepage/events/upcoming/+server.ts
@@ -13,7 +13,7 @@ export async function GET() {
 
             },
             {
-                limit: 20,
+                limit: 40,
             }
         ).sort({ dateBegin: 1 })
         .toArray();
